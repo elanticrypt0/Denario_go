5f0fb390d44633cdc54a061ce3d9ae8810fb9616
elanticrypt0 <elanticrypt0@gmail.com>
2023-07-09 21:07:10 -0300

    Update: Seeder/seed read a json file and insert into db

--- src/webcore_features/seeder.go
+++ src/webcore_features/seeder.go
@@ -1,8 +1,16 @@
 package webcore_features
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/elanticrypt0/denario_go/src/models"
+	"github.com/elanticrypt0/denario_go/src/webcore/handlers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Seed(c *fiber.Ctx) error {
+	insertCategories()
 	return c.JSON("OK")
 }
 
@@ -10,7 +18,14 @@ func seedTable(table_name string) {
 
 }
 
-// this function open a .json file and return the content into Object
-func readJsonFile(file_name string) {
-
+func insertCategories() {
+	file := handlers.ReadJsonFile("categories")
+	cat := []models.Category{}
+	err := json.Unmarshal(file, &cat)
+	if err != nil {
+		log.Fatal("Cant unmarshal json", err)
+	}
+	for _, category := range cat {
+		models.CreateCategory(category.Name)
+	}
 }
